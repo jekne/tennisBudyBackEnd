4239c388ced4bdff487f68c520e0e5dd576c8a2a
Johann Eugen <jekneto@hotmail.com>
2022-04-09 13:42:07 +0200

    ts

--- routers/chatRouter.js
+++ routers/chatRouter.js
@@ -10,7 +10,7 @@ const UserMatches = require("../models").userMatches;
 const Chats = require("../models").chat;
 const authMiddleware = require("../auth/middleware");
 
-//GET ALL THE CHA
+//GET ALL THE CHATS
 //http -v GET :4000/chats
 
 router.get("/", async (req, res, next) => {
