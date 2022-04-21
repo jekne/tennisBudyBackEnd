0eba4b546f9a47dab15448e55298f272e52e997c
Johann Eugen <jekneto@hotmail.com>
2022-04-21 22:41:32 +0200

    clubs

--- routers/clubsRouter.js
+++ routers/clubsRouter.js
@@ -1,19 +1,19 @@
-// const { Router } = require("express");
-// const router = new Router();
-// const Club = require("../models/").club;
+const { Router } = require("express");
+const router = new Router();
+const Club = require("../models/").club;
 
-// router.get("/", async (req, res, next) => {
-//   try {
-//     const clubs = await Club.findAll();
-//     if (!clubs) {
-//       res.status(404).send("Something went wrong!");
-//     } else {
-//       res.status(200).send({ message: "All the clubs!!!", clubs: clubs });
-//     }
-//   } catch (e) {
-//     console.log(e);
-//     next(e);
-//   }
-// });
+router.get("/", async (req, res, next) => {
+  try {
+    const clubs = await Club.findAll();
+    if (!clubs) {
+      res.status(404).send("Something went wrong!");
+    } else {
+      res.status(200).send({ message: "All the clubs!!!", clubs: clubs });
+    }
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+});
 
-// module.exports = router;
+module.exports = router;
