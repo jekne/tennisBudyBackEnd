4852086f6f1986089ccd68d983619c21c279ba76
Johann Eugen <jekneto@hotmail.com>
2022-04-22 17:23:06 +0200

    levels checked

--- routers/levelRouter.js
+++ routers/levelRouter.js
@@ -1,47 +1,47 @@
-// const { Router } = require("express");
-// const router = new Router();
-// const User = require("../models/").user;
-// const Level = require("../models").level;
+const { Router } = require("express");
+const router = new Router();
+const User = require("../models/").user;
+const Level = require("../models").level;
 
-// router.get("/", async (req, res, next) => {
-//   try {
-//     const levels = await Level.findAll();
-//     if (!levels) {
-//       res.status(404).send("Something went wrong!");
-//     } else {
-//       res.status(200).send({ message: "All the levels!!!", levels: levels });
-//     }
-//   } catch (e) {
-//     console.log(e);
-//     next(e);
-//   }
-// });
+router.get("/", async (req, res, next) => {
+  try {
+    const levels = await Level.findAll();
+    if (!levels) {
+      res.status(404).send("Something went wrong!");
+    } else {
+      res.status(200).send({ message: "All the levels!!!", levels: levels });
+    }
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+});
 
-// router.put("/update/:id/:levelId", async (req, res, next) => {
-//   try {
-//     const id = parseInt(req.params.id);
-//     console.log("id", id);
+router.put("/update/:id/:levelId", async (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id);
+    console.log("id", id);
 
-//     const levelId = parseInt(req.params.levelId);
+    const levelId = parseInt(req.params.levelId);
 
-//     const users = await User.findByPk(id);
+    const users = await User.findByPk(id);
 
-//     if (!users) {
-//       res.status(404).send(`The id provided ${id}, was not founded`);
-//     }
+    if (!users) {
+      res.status(404).send(`The id provided ${id}, was not founded`);
+    }
 
-//     const updateUser = await users.update({
-//       levelId: levelId,
-//     });
+    const updateUser = await users.update({
+      levelId: levelId,
+    });
 
-//     res.status(200).send({
-//       message: `This is the User correspondent a id ${id},  His level was update`,
-//       updateUser: updateUser,
-//     });
-//   } catch (e) {
-//     console.log(e);
-//   }
-//   next();
-// });
+    res.status(200).send({
+      message: `This is the User correspondent a id ${id},  His level was update`,
+      updateUser: updateUser,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+  next();
+});
 
-// module.exports = router;
+module.exports = router;
