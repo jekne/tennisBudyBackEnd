ce93b90d7d0d90d51a2f6e1ab17596f0a1f15044
Johann Eugen <jekneto@hotmail.com>
2022-05-17 13:37:16 +0200

    loacation working

--- routers/locationRouter.js
+++ routers/locationRouter.js
@@ -1,21 +1,21 @@
-// const { Router } = require("express");
-// const router = new Router();
-// const Location = require("../models").location;
+const { Router } = require("express");
+const router = new Router();
+const Location = require("../models").location;
 
-// router.get("/", async (req, res, next) => {
-//   try {
-//     const locations = await Location.findAll();
-//     if (!locations) {
-//       res.status(404).send("Something went wrong!");
-//     } else {
-//       res
-//         .status(200)
-//         .send({ message: "All the locations!!!", locations: locations });
-//     }
-//   } catch (e) {
-//     console.log(e);
-//     next(e);
-//   }
-// });
+router.get("/", async (req, res, next) => {
+  try {
+    const locations = await Location.findAll();
+    if (!locations) {
+      res.status(404).send("Something went wrong!");
+    } else {
+      res
+        .status(200)
+        .send({ message: "All the locations!!!", locations: locations });
+    }
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+});
 
-// module.exports = router;
+module.exports = router;
